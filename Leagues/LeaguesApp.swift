dc5524341e95a71c594b8e19280eef76d3634e10
Asaf Huseyn <a_huseyn@icloud.com>
2024-07-18 21:39:45 +0400

    Initial Commit

---
+++ Leagues/LeaguesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaguesApp.swift
+//  Leagues
+//
+//  Created by asafhuseyn on 18.07.24.
+//
+
+import SwiftUI
+
+@main
+struct LeaguesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
